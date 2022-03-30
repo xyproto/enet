c5f9863d1c5a4d53f6c1ac6efd5b18196fe7792e
Alexander F. Rødseth <rodseth@gmail.com>
2022-03-30 11:32:29 +0200

    Update example utilities

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -8,32 +8,19 @@ import (
 	"github.com/xyproto/enet"
 )
 
-func main() {
-	endpoint := "127.0.0.1:19091"
-	c := install_signal()
-
-	host, err := enet.NewHost("")
-	panic_if_error(err)
-	host.SetDataHandler(ping_on_reliable)
-	host.SetConnectionHandler(ping_on_connect)
-	host.SetDisconnectionHandler(func(enet.Host, string, int) {
-		host.Stop()
-	})
-	host.Connect(endpoint)
-	host.Run(c)
-}
-
 func ping_on_connect(host enet.Host, ep string, reason int) {
 	if reason == 0 {
 		host.Write(ep, 0, []byte("hello enet"))
 	}
 }
+
 func ping_on_reliable(host enet.Host, ep string, chid uint8, data []byte) {
 	s := string([]byte(data))
 
 	fmt.Println(s)
 	host.Disconnect(ep)
 }
+
 func install_signal() chan os.Signal {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
@@ -44,4 +31,18 @@ func panic_if_error(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	endpoint := "127.0.0.1:19091"
+	c := install_signal()
+	host, err := enet.NewHost("")
+	panic_if_error(err)
+	host.SetDataHandler(ping_on_reliable)
+	host.SetConnectionHandler(ping_on_connect)
+	host.SetDisconnectionHandler(func(enet.Host, string, int) {
+		host.Stop()
+	})
+	host.Connect(endpoint)
+	host.Run(c)
 }
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,24 +8,14 @@ import (
 	"github.com/xyproto/enet"
 )
 
-func main() {
-	endpoint := "127.0.0.1:19091"
-	c := install_signal()
-	host, err := enet.NewHost(endpoint)
-	panic_if_error(err)
-
-	host.SetDataHandler(pong)
-	host.SetConnectionHandler(on_peer_connected)
-	host.SetDisconnectionHandler(on_peer_disconnected)
-	host.Run(c)
-}
-
 func on_peer_connected(host enet.Host, peer string, ret int) {
 	fmt.Printf("%v conn\n", peer)
 }
+
 func on_peer_disconnected(host enet.Host, peer string, ret int) {
 	fmt.Printf("%v disconn\n", peer)
 }
+
 func install_signal() chan os.Signal {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
@@ -42,4 +32,15 @@ func panic_if_error(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	endpoint := "127.0.0.1:19091"
+	c := install_signal()
+	host, err := enet.NewHost(endpoint)
+	panic_if_error(err)
+	host.SetDataHandler(pong)
+	host.SetConnectionHandler(on_peer_connected)
+	host.SetDisconnectionHandler(on_peer_disconnected)
+	host.Run(c)
 }
